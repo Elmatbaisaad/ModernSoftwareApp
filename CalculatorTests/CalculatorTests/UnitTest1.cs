97140cf1407695948ddf53b2b8cd88b64c254895
saad <elmatbaisaad@gmail.com>
2025-04-17 13:27:00 +0200

    first commit c#

---
+++ CalculatorTests/CalculatorTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using ModernSoftwareApp;
+
+namespace CalculatorTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Add_ShouldReturnCorrectSum()
+    {
+        var calc = new Calculator();
+        Assert.Equal(5, calc.Add(2, 3));
+    }
+}
